f919dc29d7f5e4c986fbba6ddc5d4842057a7db4
root <root@SaraYui.localdomain>
2026-08-15 15:35:58 -0700

    this is also printf

---
+++ qeu.c
@@ -0,0 +1,12 @@
+/*Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+
+Use the function printf
+You are not allowed to use the function puts
+Your program should return 0*/
+
+#include <stdio.h>
+
+int main () {
+    printf ("with proper grammar, but the outcome is a piece of art\n");
+    return 0;
+}
