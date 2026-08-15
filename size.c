447c4db6664a76c4127b953b57945bb487b3bc0d
root <root@SaraYui.localdomain>
2026-08-15 15:38:10 -0700

    this is also the size of things I think

---
+++ size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    int myint;
+    float myfloat;
+    double mydouble;
+    char mychar;
+    printf ("%zu\n", sizeof (myint));
+    printf ("%zu\n", sizeof (myfloat));
+     printf ("%zu\n", sizeof (mydouble));
+      printf ("%zu\n", sizeof (mychar));
+    return 0;
+}
